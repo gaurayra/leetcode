0b312306b1224919f449a59e6b45385c64a2b199
gaurayra <gaurayra819@gmail.com>
2026-09-12 19:45:44 +0530

    Time: 179 ms (35.18%), Space: 198.1 MB (38.89%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
@@ -2,7 +2,6 @@ class Solution {
     static class State {
         long score;
         List<Integer> list;
-
         State(long score, List<Integer> list) {
             this.score = score;
             this.list = list;
@@ -10,121 +9,56 @@ class Solution {
     }
     public int[] maximumWeight(List<List<Integer>> intervals) {
         int n = intervals.size();
-
-        // [start, end, weight, original index]
         int[][] a = new int[n][4];
-
         for (int i = 0; i < n; i++) {
             a[i][0] = intervals.get(i).get(0);
             a[i][1] = intervals.get(i).get(1);
             a[i][2] = intervals.get(i).get(2);
             a[i][3] = i;
         }
-
-        // Sort according to starting point
         Arrays.sort(a, (x, y) -> Integer.compare(x[0], y[0]));
-
         State[][] dp = new State[n + 1][5];
-
-        // If we can select 0 intervals, answer is empty
         for (int i = 0; i <= n; i++) {
             dp[i][0] = new State(0, new ArrayList<>());
         }
-
-        // If we reach the end, answer is empty
         for (int k = 1; k <= 4; k++) {
             dp[n][k] = new State(0, new ArrayList<>());
         }
-
-        // DP
         for (int i = n - 1; i >= 0; i--) {
-
             for (int k = 1; k <= 4; k++) {
-
-                // OPTION 1: Don't take this interval
                 State skip = dp[i + 1][k];
-
-                // OPTION 2: Take this interval
                 int next = findNext(a, i);
-
                 State after = dp[next][k - 1];
-
-                List<Integer> takeList =
-                        new ArrayList<>(after.list);
-
+                List<Integer> takeList =new ArrayList<>(after.list);
                 takeList.add(a[i][3]);
-
                 Collections.sort(takeList);
-
-                long takeScore =
-                        a[i][2] + after.score;
-
-                State take =
-                        new State(takeScore, takeList);
-
-                // Choose the better answer
-                if (take.score > skip.score) {
-                    dp[i][k] = take;
-                }
-                else if (take.score < skip.score) {
-                    dp[i][k] = skip;
-                }
-                else {
-                    dp[i][k] = smaller(take, skip);
-                }
+                long takeScore =a[i][2] + after.score;
+                State take =new State(takeScore, takeList);
+                if (take.score > skip.score) dp[i][k] = take;
+                else if (take.score < skip.score) dp[i][k] = skip;
+                else dp[i][k] = smaller(take, skip);
             }
         }
-
         List<Integer> answer = dp[0][4].list;
-
         Collections.sort(answer);
-
-        return answer.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
-
-
-    // Find first interval whose start > current end
     private int findNext(int[][] a, int i) {
-
         int left = i + 1;
         int right = a.length;
-
         int end = a[i][1];
-
         while (left < right) {
-
             int mid = (left + right) / 2;
-
-            if (a[mid][0] > end) {
-                right = mid;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-
-        return left;
+            if (a[mid][0] > end) right = mid;
+            else left = mid + 1;
+        } return left;
     }
-
-
-    // If scores are equal, choose lexicographically smaller list
     private State smaller(State a, State b) {
-
         for (int i = 0; i < a.list.size(); i++) {
-
             if (!a.list.get(i).equals(b.list.get(i))) {
-
-                if (a.list.get(i) < b.list.get(i)) {
-                    return a;
-                }
-                else {
-                    return b;
-                }
+                if (a.list.get(i) < b.list.get(i))  return a;
+                else return b;
             }
-        }
-
-        return a;
+        } return a;
     }
 }
